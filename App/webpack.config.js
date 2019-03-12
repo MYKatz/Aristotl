ff637804148eab047bd30cd1bb290a694a345296
M. Y. Katz <mykatz01@gmail.com>
2019-03-11 22:00:43 -0700

    Finally, everything working relatively well... we're in business, baby!

---
+++ App/webpack.config.js
@@ -0,0 +1,23 @@
+var path = require('path');
+var webpack = require('webpack');
+module.exports = {
+ entry: './client/index.js',
+ output: {
+  path: path.join(__dirname, 'client'),
+  filename: 'bundle.js'
+ },
+ module: {
+  rules: [{
+   test: /.jsx?$/,
+   loader: 'babel-loader',
+   exclude: /node_modules/,
+   query: {
+    presets: ["@babel/preset-env","@babel/preset-react"]
+   }
+  },
+  {
+   test: /\.css$/,
+   loader: "style-loader!css-loader"
+  }]
+ }
+}
