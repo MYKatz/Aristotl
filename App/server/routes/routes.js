d082b46e7f4cc0c4114f505ec5671c3219c4f4fc
M. Y. Katz <mykatz01@gmail.com>
2019-03-13 21:09:56 -0700

    Changed titled, added favicon

--- App/server/routes/routes.js
+++ App/server/routes/routes.js
@@ -1,5 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+
+router.get('/favicon', function(req, res){
+  res.sendFile(path.join(__dirname, '../../client/favicon.png'))
+});
+
 router.get('/*', function(req, res){
   res.render('index')
 });
