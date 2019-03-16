2e291b58b8344f6ab342937fce9d30774ae8b1a4
M. Y. Katz <mykatz01@gmail.com>
2019-03-15 19:43:35 -0700

    Google login

--- App/client/components/Login.js
+++ App/client/components/Login.js
@@ -14,6 +14,7 @@ class Login extends Component {
     this.login = this.login.bind(this);
     this.logout = this.logout.bind(this);
     this.fblogin = this.fblogin.bind(this);
+    this.glogin = this.glogin.bind(this);
   }
 
   async checkAuthentication() {
@@ -28,7 +29,7 @@ class Login extends Component {
   }
 
   async login() {
-    // Redirect to '/' after login
+    // Redirect to '/dash' after login
     this.props.auth.login('/dash');
   }
 
@@ -38,7 +39,13 @@ class Login extends Component {
   }
 
   async fblogin() {
+    //FB login w/ idp ID from Okta
     this.props.auth.login('/dash', {idp: 	"0oacyiupocl7wF23w356"});
+  }
+
+  async glogin() {
+    //FB login w/ idp ID from Okta
+    this.props.auth.login('/dash', {idp: 	"0oacyltq2wgHWCUaS356"});
   }
 
   render() {
@@ -57,7 +64,7 @@ class Login extends Component {
                 <div>
                   <button className="button is-medium is-fullwidth blackbutton" onClick={this.login}>Login</button>
                   <a href="https://dev-994297.okta.com/signin/register" className="button is-medium is-fullwidth blackbutton">Register</a>
-                  <button className="button is-danger is-outlined is-fullwidth" style={{marginBottom: "1vh"}}>Sign-in with Google</button>
+                  <button className="button is-danger is-outlined is-fullwidth" onClick={this.glogin} style={{marginBottom: "1vh"}}>Sign-in with Google</button>
                   <a className="button is-link is-outlined is-fullwidth" onClick={this.fblogin}>Sign-in with Facebook</a>
                 </div>
               </div>
--- notes.md
+++ notes.md
@@ -18,3 +18,12 @@
 ## Okta Integration
 * Should use both User/pass login and social login... and perhaps some more advanced integration w/ social media like FB messenger, etc.
 * SMS verification as well for chatbot will score a bunch of points imo
+
+## Okta features used
+* Auth/login (obviously)
+* External identity providers (IDP) through Okta - Facebook, Google
+
+### Potential Okta integrations
+* Webhooks - userdata creation on the back-end
+* Custom sign-in page w/ custom domain (should do if you have the chance!)
+* Multi-factor login
