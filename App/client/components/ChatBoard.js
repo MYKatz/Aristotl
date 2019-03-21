53e41ce74833b032561f23ec42ddea4e64070fd4
M. Y. Katz <mykatz01@gmail.com>
2019-03-20 17:06:16 -0700

    new chat UI and scrollbar

--- App/client/components/ChatBoard.js
+++ App/client/components/ChatBoard.js
@@ -4,24 +4,18 @@ import CanvasDraw from "react-canvas-draw";
 import { withAuth } from '@okta/okta-react';
 import { withRouter } from 'react-router-dom';
 import openSocket from 'socket.io-client';
-
-import MessageBubble from "./elements/MessageBubble";
+import { ChatFeed, Message } from 'react-chat-ui';
 
 class ChatBoard extends Component{
     constructor(props){
         super(props);
         this.updateMessages = this.updateMessages.bind(this);
-        this.updateWhiteboard = this.updateWhiteboard.bind(this);
         this.sendMessage = this.sendMessage.bind(this);
-        this.messagesScrollToBottom = this.messagesScrollToBottom.bind(this);
         this._handleKeyPress = this._handleKeyPress.bind(this);
         this._handleChange = this._handleChange.bind(this);
-        this._handleResize = this._handleResize.bind(this);
-        this._handleClick = this._handleClick.bind(this);
         this._handleUp = this._handleUp.bind(this);
         this.getToken = this.getToken.bind(this);
         this.goTo = this.goTo.bind(this);
-        this.whiteboardRef = React.createRef();
         this.socket = openSocket('http://localhost:8001');
         this.state = {
             messages : [],
@@ -34,37 +28,15 @@ class ChatBoard extends Component{
     }
 
     updateMessages(msg){
-        var msglist = [...this.state.messages, msg].map((message) =>
-            <MessageBubble text={message} type="othermsg" />
-        )
-        console.log(msglist);
-        this.setState({messagejsx: msglist}, this.messagesScrollToBottom());
-        this.setState({messages: [...this.state.messages, msg]});
+        this.setState({messages: [...this.state.messages, new Message({id: 1, message: msg})]});
     }
 
     sendMessage(msg){
-        var msglist = [...this.state.messages, msg].map((message) =>
-            <MessageBubble text={message} type="usermsg" />
-        )
-        this.setState({messagejsx: msglist}, this.messagesScrollToBottom());
-        this.setState({messages: [...this.state.messages, msg]});
+        this.setState({messages: [...this.state.messages, new Message({id: 0, message: msg})]});
         this.setState({inputvalue: ""});
     }
 
-    updateWhiteboard(drawing){
-        this.whiteboardRef.loadSaveData(drawing, true);
-    }
-
-    messagesScrollToBottom(){
-        this.messagesEnd.scrollIntoView();
-    }
-
-    componentDidUpdate() {
-        this.messagesScrollToBottom();
-    }
-
     componentDidMount() {
-        this._handleResize();
         window.addEventListener('resize', this._handleResize);
         //socket handling
         this.socket.on('chat', this.updateMessages);
@@ -118,43 +90,17 @@ class ChatBoard extends Component{
         this.setState({inputvalue: c.target.value});
     }
 
-    _handleClick(c){
-        //handle right click
-        if(c.type === "contextmenu"){
-            c.preventDefault();
-            this.whiteboardRef.clear();
-        }
-        return false;
-    }
-
-    _handleResize(){
-        this.setState({
-            whiteboardHeight: document.getElementById('wb').clientHeight,
-            whiteboardWidth: document.getElementById('wb').clientWidth,
-        });
-    }
-
     render() {
         return(
             <div className="columns is-fullheight">
                 <div className="column chatinterface">
-                    <div className="boardContainer">
-                        <div className="msgBoard">
-                            <div className="msgContent">
-                                {this.state.messagejsx}
-                            </div>
-                        </div>
-                        <div style={{ float:"left", clear: "both" }}
-                            ref={(el) => { this.messagesEnd = el; }}>
-                        </div>
-                    </div>
+                    <ChatFeed 
+                            messages={this.state.messages} 
+                            showSenderName
+                    />
                     <div className="inputbox">
                         <input className="input is-rounded typemsg" type="text" value={this.state.inputvalue} onChange={this._handleChange} onKeyPress={this._handleKeyPress} placeholder="Type a message..."/>
                     </div>
-                </div>
-                <div className="column whiteboard" id="wb" onMouseUp={this._handleUp} onContextMenu={this._handleClick}>
-                    <CanvasDraw ref={canvasDraw => (this.whiteboardRef = canvasDraw)} canvasHeight={this.state.whiteboardHeight} canvasWidth={this.state.whiteboardWidth} brushRadius={2} lazyRadius={0}/>
-
                 </div>
             </div>
         )
