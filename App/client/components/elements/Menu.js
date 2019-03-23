7c0dfd8103a6d805558a0dbe524ac0278d4da25c
M. Y. Katz <mykatz01@gmail.com>
2019-03-23 12:47:14 -0700

    checkpoint... menus and stuff kinda works

--- App/client/components/elements/Menu.js
+++ App/client/components/elements/Menu.js
@@ -2,6 +2,42 @@ import React, { Component } from 'react';
 import { withAuth } from '@okta/okta-react';
 import MenuItem from "./MenuItem";
 import { withRouter } from "react-router-dom";
+
+import { withStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+
+const drawerWidth = 240;
+
+const styles = theme => ({
+    root: {
+      display: 'flex',
+    },
+    appBar: {
+      zIndex: theme.zIndex.drawer + 1,
+    },
+    drawer: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+    drawerPaper: {
+      width: drawerWidth,
+    },
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing.unit * 3,
+    },
+    toolbar: theme.mixins.toolbar,
+});
+
 class Menu extends Component {
     constructor(props){
         //accepts this.props.items as list of menu titles
@@ -41,7 +77,7 @@ class Menu extends Component {
         this.props.auth.logout('/');
     }
 
-    render(){
+    oldrender(){
         return(
             <aside className="menu">
                 <ul className="menu-list">
@@ -51,6 +87,41 @@ class Menu extends Component {
             </aside>
         )
     }
+
+    render(){
+        const { classes } = this.props;
+        return(
+            <div>
+            <CssBaseline />
+            <Drawer
+            className={classes.drawer}
+            variant="permanent"
+            classes={{
+                paper: classes.drawerPaper,
+            }}
+            >
+                <div className={classes.toolbar} />
+                <List>
+                    {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                    <ListItem button key={text}>
+                        <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                        <ListItemText primary={text} />
+                    </ListItem>
+                    ))}
+                </List>
+                <Divider />
+                <List>
+                    {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                    <ListItem button key={text}>
+                        <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                        <ListItemText primary={text} />
+                    </ListItem>
+                    ))}
+                </List>
+            </Drawer>
+            </div>
+        )
+    }
 }
 
-export default withRouter(withAuth(Menu));
+export default withStyles(styles)(withRouter(withAuth(Menu)));
